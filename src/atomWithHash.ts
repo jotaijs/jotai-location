7190effd9372e91a45fa53f0e5b96e4b4583078b
joetis06 <58756900+joetis06@users.noreply.github.com>
2023-10-14 20:05:43 -0400

    Update atomWithHash replaceState function to use window.history.state instead of null (#24)
    My project uses the new NextJS app router, and we were using atomWithHash to keep track of state. This was breaking when used with the app router when using the back button in the browser. In my project I am just setting the setHash function to do what I committed here. I figured it would be better and easier for anyone else using this library to just set the flag replaceState if they are using the nextjs app router. (Maybe the website/documentation should be updated as well.)
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- src/atomWithHash.ts
+++ src/atomWithHash.ts
@@ -42,7 +42,7 @@ export function atomWithHash<Value>(
   if (setHashOption === 'replaceState') {
     setHash = (searchParams) => {
       window.history.replaceState(
-        null,
+        window.history.state,
         '',
         `${window.location.pathname}${window.location.search}#${searchParams}`,
       );
