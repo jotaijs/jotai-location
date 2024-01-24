493e96e1bc65e9eb565a36c8b73d710da16cddf5
Filip Lindahl <filip.g.lindahl@gmail.com>
2024-01-24 19:14:53 +0100

    initial commit

---
+++ examples/04_react_router/src/index.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
