9ec977f8ab44bf90c7907515022a9c8a1b7238ed
Filip Lindahl <filip.g.lindahl@gmail.com>
2024-02-11 17:01:22 +0100

    first iteration of test for atomWithLocation

--- __tests__/atomWithLocation_spec.tsx
+++ __tests__/atomWithLocation_spec.tsx
@@ -1,7 +1,119 @@
+import { useAtom } from 'jotai';
+import React, { StrictMode } from 'react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { atomWithLocation } from '../src/index';
 
-describe.skip('atomWithLocation', () => {
-  // FIXME please someone help me with this test
+describe('atomWithLocation', () => {
+  beforeEach(() => {
+    window.history.replaceState(null, '', '/');
+  });
+
+  it('can replace state', async () => {
+    const locationAtom = atomWithLocation({ replace: true });
+
+    const Navigation = () => {
+      const [location, setLocation] = useAtom(locationAtom);
+      return (
+        <>
+          <div> current pathname in atomWithLocation: {location.pathname} </div>
+          <button type="button" onClick={() => window.history.back()}>
+            back
+          </button>
+          <button type="button" onClick={() => setLocation({ pathname: '/1' })}>
+            button1
+          </button>
+          <button
+            type="button"
+            onClick={() =>
+              setLocation({
+                pathname: '/2',
+              })
+            }
+          >
+            button2
+          </button>
+        </>
+      );
+    };
+
+    const { findByText, getByText } = render(
+      <StrictMode>
+        <Navigation />
+      </StrictMode>,
+    );
+
+    await findByText('current pathname in atomWithLocation: /');
+    expect(window.location.pathname).toEqual('/');
+    expect(window.history.length).toEqual(1);
+
+    await userEvent.click(getByText('button1'));
+
+    await findByText('current pathname in atomWithLocation: /1');
+    expect(window.location.pathname).toEqual('/1');
+    expect(window.history.length).toEqual(1);
+
+    await userEvent.click(getByText('button2'));
+    expect(window.location.pathname).toEqual('/2');
+    expect(window.history.length).toEqual(1);
+  });
+
+  it('can push state', async () => {
+    const locationAtom = atomWithLocation({ replace: false });
+
+    const Navigation = () => {
+      const [location, setLocation] = useAtom(locationAtom);
+      return (
+        <>
+          <div> current pathname in atomWithLocation: {location.pathname} </div>
+          <button type="button" onClick={() => window.history.back()}>
+            back
+          </button>
+          <button type="button" onClick={() => setLocation({ pathname: '/1' })}>
+            button1
+          </button>
+          <button
+            type="button"
+            onClick={() =>
+              setLocation({
+                pathname: '/2',
+              })
+            }
+          >
+            button2
+          </button>
+        </>
+      );
+    };
+
+    const { findByText, getByText } = render(
+      <StrictMode>
+        <Navigation />
+      </StrictMode>,
+    );
+
+    await findByText('current pathname in atomWithLocation: /');
+    expect(window.location.pathname).toEqual('/');
+    expect(window.history.length).toEqual(1);
+
+    await userEvent.click(getByText('button1'));
+
+    await findByText('current pathname in atomWithLocation: /1');
+    expect(window.location.pathname).toEqual('/1');
+    expect(window.history.length).toEqual(2);
+
+    await userEvent.click(getByText('button2'));
+
+    await findByText('current pathname in atomWithLocation: /2');
+    expect(window.location.pathname).toEqual('/2');
+    expect(window.history.length).toEqual(3);
+
+    await userEvent.click(getByText('back'));
+
+    await findByText('current pathname in atomWithLocation: /1');
+    expect(window.location.pathname).toEqual('/1');
+    expect(window.history.length).toEqual(3);
+  });
 });
 
 describe('atomWithLocation without window', () => {
